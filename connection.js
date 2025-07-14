a6f1e12bf8b40fb94d96ab98af9b962fa8864a9b
Jose Carmona <josshue85@gmail.com>
2025-07-13 23:19:38 -0400

    modificando password para conectar con la bd local e instalando dependencias

--- connection.js
+++ connection.js
@@ -4,7 +4,8 @@ const { Pool } = pg;
 const pool = new Pool({
   host: "localhost",
   user: "postgres",
-  password: "06272013",
+  // password: "06272013",
+  password: "Postgres4321@",
   database: "marketplace",
   port: 5432,
   allowExitOnIdle: true,
