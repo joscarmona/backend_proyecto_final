e1fbca239314d7ea420816f5c26191ad886400ea
Jose Carmona <josshue85@gmail.com>
2025-07-31 23:43:10 -0400

    modificación del archivo connection.js

--- connection.js
+++ connection.js
@@ -1,15 +1,34 @@
 import pg from "pg";
 const { Pool } = pg;
 
+// const pool = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   // password: "06272013",
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: 5432,
+//   allowExitOnIdle: true,
+// });
+
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  // password: "06272013",
-  password: "Postgres4321@",
-  database: "marketplace",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   allowExitOnIdle: true,
 });
+
+// const pool = new Pool({
+//   host: "dpg-d24p4qpr0fns73db3s7g-a",
+//   user: "db_marketplace_thzd_user",
+//   // password: "06272013",
+//   password: "EiyiXdNMLNmpZLbJSi3kn9nSZCcLitF0",
+//   database: "db_marketplace_thzd",
+//   port: 5432,
+//   allowExitOnIdle: true,
+// });
 
 export { pool };
 
