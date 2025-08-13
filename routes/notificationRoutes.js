ca45ee40cf30de1dfde0a5961b61754b69996459
Jose Carmona <josshue85@gmail.com>
2025-08-12 23:32:24 -0400

    retornando al punto anterior

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.get('/', authenticateToken, getNotifications);
 
 // POST /api/notifications           // authenticate
-router.post('/notifications', createNotification);
+router.post('/notifications', authenticate, createNotification);
 
 
 // PUT /api/notifications/:notificationId/read
