18ebbb123dde74841d4ad38c0edbe1e7dab69ea4
Jose Carmona <josshue85@gmail.com>
2025-08-13 00:24:33 -0400

    modificando crear notificacion 00:24

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.get('/', authenticateToken, getNotifications);
 
 // POST /api/notifications           // authenticate
-router.post('/notifications', authenticateToken, createNotification);
+router.post('/notifications', createNotification);
 
 
 // PUT /api/notifications/:notificationId/read
